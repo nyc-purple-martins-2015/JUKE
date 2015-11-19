db3caee5d3aa442e6160e495be8acc585e3df9db
jonstar <jsskis7@gmail.com>
2015-11-19 18:05:55 -0500

    finish basic rails setup

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,18 @@ require 'spec_helper'
 require 'rspec/rails'
 # Add additional requires below this line. Rails is not loaded until this point!
 
+require 'capybara/rails'
+require 'simplecov'
+# Add additional requires below this line. Rails is not loaded until this point!
+# save to CircleCI's artifacts directory if we're on CircleCI
+if ENV['CIRCLE_ARTIFACTS']
+  dir = File.join(ENV['CIRCLE_ARTIFACTS'], "coverage")
+  SimpleCov.coverage_dir(dir)
+end
+
+SimpleCov.start
+
+
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
