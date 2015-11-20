4f8ed8996d4ef32b36ad40f88919fe4b9391d67e
Corey <email": null>
2015-11-20 15:03:19 -0500

    start jquery calls

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,15 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+$(document).ready(function() {
+
+  $(".join_juke_button").on("submit", function(event){
+    event.preventdefault();
+
+  $( ".join_juke_button" ).click(function() {
+  $( ".join_juke_form" ).toggle();
+    // Animation complete.
+  });
+  });
+
+});
