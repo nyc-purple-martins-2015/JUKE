78956af1492cfda0c3a97641b61d07b9f183dfef
Lawson <lawsonmarlowe@gmail.com>
2015-11-19 22:47:10 -0500

    add user has_many votes associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,5 +5,5 @@ class User < ActiveRecord::Base
   has_many :setlists
   has_many :guest_setlists
   has_many :viewable_lists, through: :guest_setlists, source: :setlist, class_name: "Setlist"
-
+  has_many :votes
 end
