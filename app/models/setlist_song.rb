ecf21045564e68f22a280b99f18abd388e9a3329
Tahrim <utahrim@gmail.com>
2015-11-20 11:15:43 -0500

    adds validations to the models

--- app/models/setlist_song.rb
+++ app/models/setlist_song.rb
@@ -2,4 +2,6 @@ class SetlistSong < ActiveRecord::Base
   belongs_to :song
   belongs_to :setlist
   has_many :votes
+
+  validates_presence_of :song, :setlist
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ActiveRecord::Base
   has_many :guest_setlists
   has_many :viewable_lists, through: :guest_setlists, source: :setlist, class_name: "Setlist"
   has_many :votes
+
+  validates_presence_of :username, :password_digest, :email
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create
+  validates_uniqueness_of :email, :username
 end
