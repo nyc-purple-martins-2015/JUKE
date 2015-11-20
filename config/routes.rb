6426b3201f7c66658bfdf387d23d768242fd3181
jonstar <jsskis7@gmail.com>
2015-11-20 10:43:17 -0500

    add basic resources and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,11 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
+  root 'index#home'
 
+  resources :jukes
+  resources :users
+  resources :sessions, only: [:create, :destroy]
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
 
