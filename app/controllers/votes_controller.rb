9ebcae431e2895a7ffde842834b95151c71e68b3
Corey <email": null>
2015-11-21 17:16:13 -0500

    use vote method to limit votes to 1 per setlist_song

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,8 +3,9 @@ class VotesController < ApplicationController
 def create
   @user = current_user
   @vote = Vote.new(vote_params)
+  @setlist_song = SetlistSong.find(@vote.setlist_song_id)
   @vote.user_id = @user.id
-  if @vote.save
+  if @vote.first_vote?(@user, @setlist_song) && @vote.save
     redirect_to setlist_path(@vote.setlist_song.setlist_id)
   else
     redirect_to login_path
--- app/models/vote.rb
+++ app/models/vote.rb
@@ -3,4 +3,9 @@ class Vote < ActiveRecord::Base
   belongs_to :setlist_song
 
   validates_presence_of :user, :setlist_song, :value
+
+  def first_vote?(user, setlist_song)
+    user.votes.count(setlist_song_id = setlist_song.id) == 0
+  end
+
 end
