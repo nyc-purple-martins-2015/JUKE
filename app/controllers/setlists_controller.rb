7e7b98c51c81a5b79fb0b50a42ac2a995287c532
Jon Star & Tahrim Uddin <pair+jonathanstar.utahrim@devbootcamp.com>
2015-11-21 01:04:36 -0500

    add first part of setlist edit controller complete with correct spotify calls, a view, and some basic styling

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -6,16 +6,22 @@ class SetlistsController < ApplicationController
 
   def edit
     @setlist= Setlist.find(params[:id])
-    playlists = spotify_get("https://api.spotify.com/v1/users/#{current_user.uid}/playlists")
-    json_playlists = JSON.parse(playlists.body)
-
-    our_pretend_playlist = json_playlists["items"][3]
-    our_pretend_playlist_url = our_pretend_playlist["href"]
-    json_opp = spotify_get(our_pretend_playlist_url)
-    parsed = JSON.parse(json_opp.body)
+    json = spotify_get(@setlist.list_spotify_url)
+    parsed = JSON.parse(json.body)
     t_array = parsed["tracks"]["items"]
 
-    @tracks = t_array.map { |t| t["track"]["name"] }
+    @tracks = t_array.map do |t|
+
+      artist_list = []
+      t["track"]["artists"].each { |artist| artist_list << artist["name"] }
+
+      {
+        name: t["track"]["name"],
+        song_spotify_url: t["track"]["href"],
+        album: t["track"]["album"]["name"],
+        artist: artist_list.join(", ")
+      }
+    end
   end
 
 
