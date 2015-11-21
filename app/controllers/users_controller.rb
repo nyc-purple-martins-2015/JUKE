c4a53455a738db00715ed0c1eaea3ea282150c1c
Corey <email": null>
2015-11-21 14:47:00 -0500

    fix login bug

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,7 @@ class UsersController < ApplicationController
 
   def create
      @user = User.new(user_params)
+     @user.provider = LOCAL_LOGIN
     if @user.save
       session[:user_id] = @user.id
       redirect_to root_path
