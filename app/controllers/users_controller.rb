622d82d57878df465a9ee4b562fb33796dff23df
Lawson <lawsonmarlowe@gmail.com>
2015-11-21 13:49:13 -0500

    add registration functionality

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,19 @@
 class UsersController < ApplicationController
+
+  def create
+     @user = User.new(user_params)
+    if @user.save
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to login_path
+    end
+  end
+
+    private
+     def user_params
+      params.require(:user).permit(:username, :email, :password)
+    end
+
 end
 
