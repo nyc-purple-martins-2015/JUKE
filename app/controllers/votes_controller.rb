a8079e99d645125df1c3bc6755988c0dfab688a7
Corey <email": null>
2015-11-21 16:38:37 -0500

    add votes controller and vote value view, successfully create votes

---
+++ app/controllers/votes_controller.rb
@@ -0,0 +1,20 @@
+class VotesController < ApplicationController
+
+def create
+  @user = current_user
+  @vote = Vote.new(vote_params)
+  @vote.user_id = @user.id
+  if @vote.save
+    redirect_to setlist_path(@vote.setlist_song.setlist_id)
+  else
+    redirect_to login_path
+  end
+end
+
+private
+
+  def vote_params
+    params.require(:vote).permit(:value, :setlist_song_id)
+  end
+
+end
