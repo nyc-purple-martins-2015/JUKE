49ef537f7705dd7875260426383ab6c879bf4877
Rebecca Kleinberg <email": null>
2015-11-21 21:34:34 -0500

    user #create tests pass

--- spec/controllers/user_controller_spec.rb
+++ spec/controllers/user_controller_spec.rb
@@ -8,7 +8,24 @@ RSpec.describe UsersController do
       expect{
         post :create, user: FactoryGirl.attributes_for(:user)
       }.to change(User, :count).by(1)
+    end
 
+    it 'does not create a user when params are invalid' do
+      expect{
+        post :create, user: FactoryGirl.attributes_for(:setlist)
+      }.to_not change(User, :count)
+    end
+
+    it 'redirects to the root_path when the user is successfully created' do
+      expect(
+        post :create, user: FactoryGirl.attributes_for(:user)
+      ).to redirect_to root_path
+    end
+
+    it 'redirects to login_path when user is unsuccessfully created' do
+      expect(
+        post :create, user: FactoryGirl.attributes_for(:setlist)
+      ).to redirect_to login_path
     end
 
   end
@@ -35,24 +52,6 @@ end
 
 
   # context '#create' do
-
-  #   it 'creates a new user with valid params' do
-  #     expect{
-  #       post :create, user: FactoryGirl.attributes_for(:user)
-  #     }.to change(User, :count).by(1)
-  #   end
-
-  #   it 'does not create create a user when params are invalid' do
-  #     expect{
-  #       post :create, user: FactoryGirl.attributes_for(:question)
-  #     }.to_not change(User, :count)
-  #   end
-
-  #   it 'redirects to the root_path when the user is successfully created' do
-  #     expect(
-  #         post :create, user: FactoryGirl.attributes_for(:user)
-  #     ).to redirect_to root_path
-  #   end
 
   #   it 'redirects to the root_path when the user is successfully created' do
   #     expect(
