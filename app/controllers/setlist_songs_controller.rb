9f24988dd98727c2b4f434362e54b000fe1c446c
Tahrim Uddin <utahrim@gmail.com>
2015-11-22 00:19:40 -0500

    this shit is not working

--- app/controllers/setlist_songs_controller.rb
+++ app/controllers/setlist_songs_controller.rb
@@ -1,8 +1,14 @@
 class SetlistSongsController < ApplicationController
 
-def update
+  def update
 
-end
+    # params.each do |param|
+    #   if
 
+  end
+
+  def setlist_songs_params
+    params.require(:setlist_song).permit(:setlist_song_id)
+  end
 
 end
