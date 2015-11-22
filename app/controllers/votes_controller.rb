63fafaa54a00cf2925531c4fbe769b68db3b29c9
Corey <email": null>
2015-11-21 19:10:19 -0500

    refactor and add volidation

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,14 +1,13 @@
 class VotesController < ApplicationController
 
 def create
-  @user = current_user
   @vote = Vote.new(vote_params)
   @setlist_song = SetlistSong.find(@vote.setlist_song_id)
-  @vote.user_id = @user.id
-  if @vote.first_vote?(@user, @setlist_song) && @vote.save
+  @vote.user = current_user
+  if @vote.save
     redirect_to setlist_path(@setlist_song)
   else
-    flash[:alert] = "You have already voted for this song!"
+    flash[:alert] = "you already voted"
     redirect_to setlist_path(@setlist_song)
   end
 end
@@ -18,5 +17,4 @@ private
   def vote_params
     params.require(:vote).permit(:value, :setlist_song_id)
   end
-
 end
