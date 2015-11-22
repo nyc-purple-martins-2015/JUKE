1ec5efa5694e6b6db6364d054f01322cde249bf3
Corey <email": null>
2015-11-22 16:09:57 -0500

    working welcome e-mail

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -14,7 +14,15 @@ Rails.application.configure do
   config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.smtp_settings = {
+   :address              => "smtp.gmail.com",
+   :port                 => 587,
+   :user_name            => ENV['gmail_username'],
+   :password             => ENV['gmail_pw'],
+   :authentication       => "plain",
+  :enable_starttls_auto => true
+  }
 
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
