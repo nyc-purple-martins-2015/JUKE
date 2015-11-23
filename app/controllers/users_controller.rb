278bce02f5056605ec754305659631c283ddde94
Rebecca Kleinberg <email": null>
2015-11-22 20:06:03 -0500

    logged out users denied access to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,12 @@
 class UsersController < ApplicationController
 
   def show
-    @user = User.find(params[:id])
+    if logged_in?
+      @user = User.find(params[:id])
+    else
+      flash[:alert] = "Log in to access JUKE"
+      redirect_to root_path
+    end
   end
 
   def create
