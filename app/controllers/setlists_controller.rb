3263b7271a1c9daa447c30b38aa6e153984859f3
jonstar <jsskis7@gmail.com>
2015-11-24 15:48:48 -0500

    build method for finding position of setlist_song in actual spotify playlist

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -74,6 +74,9 @@ class SetlistsController < ApplicationController
 
   def reorder
     @setlist = Setlist.find(params[:id])
+    response = SpotifyPlaylistGetter.new(session[:token], setlist: @setlist).get
+    json = JSON.parse(response.body)
+
     SpotifyReorderPutter.new(session[:token], {user: current_user, setlist: @setlist, request_type: "put", range_start: 14, insert_before: 0, range_length: 1}).post
     redirect_to setlist_path(@setlist)
   end
