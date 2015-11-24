81f81a660f9696b20f35842386415fbf3b6b956b
Rebecca Kleinberg <email": null>
2015-11-24 16:13:36 -0500

    Jukebox works

--- app/controllers/setlists_controller.rb
+++ app/controllers/setlists_controller.rb
@@ -83,9 +83,6 @@ class SetlistsController < ApplicationController
 
   def player
     @setlist = Setlist.find(params[:setlist_id])
-    res = SpotifyPlaylistsGetter.new(session[:token], user: current_user).get
-    @playlists = SpotifyGetter.parse_playlists(res)
-    # binding.pry
   end
 
   private
