040daf60313548ad9097361b65d9b1824d3c807a
Tahrim Uddin <utahrim@gmail.com>
2015-11-23 20:53:51 -0500

    Ajax invite form

--- app/assets/javascripts/invite.js
+++ app/assets/javascripts/invite.js
@@ -1,17 +1,19 @@
-// $(document).ready(function() {
+$(document).ready(function() {
 
-//   $(".invite_button").on("submit", function(event) {
-//      event.preventDefault();
-
-//       var request = $.ajax({
-//       type: "get",
-//       url: "send_invites",
-//       data: $(this).serialize()
-//       })
-
-//       request.done(function(response) {
-//         $(".invite_button").html(response);
-
-//     });
-//   });
-// });
+  $(".invite_button").on("submit", ".button_to", function(event) {
+     event.preventDefault();
+    if ($(".invite_form").is(':visible')){
+        $(".invite_form").remove();
+      }
+    else {
+      var request = $.ajax({
+      type: "get",
+      url: "invite",
+      data: $(this).serialize()
+      })
+    request.done(function(response) {
+        $(".invite_button").append(response);
+      });
+    }
+  });
+});
