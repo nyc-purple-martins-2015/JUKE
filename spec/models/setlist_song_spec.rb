00d9ee7d8efc6d14a49b90e76312c7bced76a59f
Tahrim Uddin <utahrim@gmail.com>
2015-11-25 12:29:24 -0500

    working on setlist_song tests

--- spec/models/setlist_song_spec.rb
+++ spec/models/setlist_song_spec.rb
@@ -5,4 +5,15 @@ RSpec.describe SetlistSong, type: :model do
     it { should validate_presence_of :song}
     it { should validate_presence_of :setlist}
   end
+
+  context "#vote_count" do
+    s_song = FactoryGirl.build :setlist_song
+
+     s_song.votes << Vote.create!(user: FactoryGirl.build(:user), setlist_song: FactoryGirl.build(:setlist_song), value: 1)
+     binding.pry
+
+    it "should return the sum of the votes value" do
+      expect(s_song.vote_count).to eq(1)
+    end
+  end
 end
--- spec/models/setlist_spec.rb
+++ spec/models/setlist_spec.rb
@@ -7,4 +7,11 @@ RSpec.describe Setlist, type: :model do
     it { should validate_presence_of :spotify_url}
     it { should validate_presence_of :invite_code}
   end
+
+  context "#spotify_id" do
+    let(:setlist) {FactoryGirl.build :setlist}
+    it "should not return a string of everything after the last '/'" do
+      expect(setlist.spotify_id.include?("spotify.com")).to be(false)
+    end
+  end
 end
