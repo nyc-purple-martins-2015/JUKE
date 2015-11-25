5ef0dfa3ab36d48e3e342d40e0741098ff8a63fc
Corey <email": null>
2015-11-25 14:31:19 -0500

    add methods to migration

--- db/migrate/20151125162727_change_url_attrs_to_same.rb
+++ db/migrate/20151125162727_change_url_attrs_to_same.rb
@@ -1,4 +1,6 @@
 class ChangeUrlAttrsToSame < ActiveRecord::Migration
   def change
+    rename_column :setlists, :list_spotify_url, :spotify_url
+    rename_column :songs, :song_spotify_url, :spotify_url
   end
 end
